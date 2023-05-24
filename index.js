61c5c7cf8205553c147fe2b8f126ac8c1e0f93dc
WhidanB <danyphone95@gmail.com>
2023-05-24 14:17:41 +0200

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+// const cursor = document.getElementById("cursor");
+// const rond2 = document.getElementById("mouse1");
+// const rond3 = document.getElementById("mouse2");
+
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.x + "px";
+//   cursor.style.top = e.y + "px";
+//   rond2.style.left = e.x + "px";
+//   rond2.style.top = e.y + "px";
+//   rond3.style.left = e.x + "px";
+//   rond3.style.top = e.y + "px";
+// });
+
+const ronds = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+  ronds.forEach((rond) => {
+    rond.style.left = e.x + "px";
+    rond.style.top = e.y + "px";
+  });
+});
