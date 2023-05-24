bc86bf48b9805f890b76b0438f8fa86285ee8a1a
WhidanB <danyphone95@gmail.com>
2023-05-24 13:03:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
